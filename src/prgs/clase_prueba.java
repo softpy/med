199b36d4bf06de37bcdd406d90b6e59d507a1c76
RRHH <RRHH@RRHH-JOSEKA>
2014-08-20 09:33:43 -0400

    #2 -> Clase prueba

---
+++ src/prgs/clase_prueba.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prgs;
+
+/**
+ *
+ * @author RRHH
+ */
+public class clase_prueba {
+    
+}
